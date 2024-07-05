abf1d3e600c7ce26b04da31bbb642ecb96a3b1bd
dhodi-fahad <dhodifahad@gmail.com>
2024-07-05 07:11:46 +0300

    STACK

---
+++ src/COLLECTIONS/STACKS/stack.ts
@@ -0,0 +1,6 @@
+// Stack - natively array Last In First Out
+
+const stack = [];
+stack.push('a')
+const val1 = stack.pop() //removes from the end
+const val2 = stack[stack.length - 1] // peek at the end
